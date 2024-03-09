50fb420f8804e0e85bc95fb1ff41329689e9b581
Roman Luštrik <roman.lustrik@biolitika.si>
2024-03-09 12:15:25 +0100

    Adapt scripts to accommodate new file structure

--- plot_data.R
+++ plot_data.R
@@ -2,58 +2,90 @@ library(ggplot2)
 
 dir.create("figures", showWarnings = FALSE)
 
-load("./data/raw_accident_data_1995_2021.RData")
+load(file.path("data", "raw_accident_data.RData"))
 
-pn2004$datum <- as.Date(pn2004$datum, format = "%d.%m.%Y")
-pn2021$DatumPN <- as.Date(pn2021$DatumPN, format = "%d.%m.%Y")
+rad <- raw.accident.data
+rm(raw.accident.data)
 
-pn2021$VrednostAlkotesta <- as.numeric(gsub(",", replacement = "\\.", x = pn2021$VrednostAlkotesta))
+rad$DatumPN <- as.Date(rad$DatumPN, format = "%d.%m.%Y")
 
-pn2021$VozniskiStalez <- ((pn2021$VozniskiStazVLetih * 12) + pn2021$VozniskiStazVMesecih) / 12
+rad$VrednostAlkotesta <- as.numeric(
+  gsub(",", replacement = "\\.", x = rad$VrednostAlkotesta)
+)
+
+rad$VozniskiStalez <- ((rad$VozniskiStazVLetih * 12) + rad$VozniskiStazVMesecih) / 12
 
 # These NAs result due to incomplete final line in each file.
-pn2004 <- pn2004[!is.na(pn2004$datum), ]
-
-pn2004$year <- strftime(pn2004$datum, format = "%Y")
-pn2021$year <- strftime(pn2021$DatumPN, format = "%Y")
+rad$year <- strftime(rad$DatumPN, format = "%Y")
 
 # Remove unknown gender/sex.
-pn2021 <- pn2021[pn2021$Spol %in% c("MOŠKI", "ŽENSKI"), ]
+rad <- rad[rad$Spol %in% c("MOŠKI", "ŽENSKI"), ]
 
 ggplot(
-  data = pn2021,
+  data = rad,
   mapping = aes(x = Spol, y = Starost)
 ) +
   theme_bw() +
   geom_violin() +
   geom_jitter(width = 0.1, alpha = 0.05) +
   facet_wrap(~KlasifikacijaNesrece)
-ggsave("figures/figure-0-accidents-by-gender-and-severity.png", 
+
+ggsave(
+  file.path("figures", "figure-0-accidents-by-gender-and-severity.png"),
        width = 1000, height = 1000, units = "px", dpi = 150)
 
-pn2021.kolo <- pn2021[pn2021$VrstaUdelezenca == "KOLESAR", ]
+kolo <- rad[rad$VrstaUdelezenca == "KOLESAR", ]
 
 ggplot(
-  data = pn2021.kolo,
+  data = kolo,
   mapping = aes(x = Povzrocitelj, y = Starost)
 ) +
   theme_bw() +
   geom_violin() +
   geom_jitter(width = 0.1, alpha = 0.1) +
   facet_wrap(~PoskodbaUdelezenca)
-ggsave("figures/figure-1-cyclists-age-structure-by-severity.png", 
-       width = 1000, height = 1000, units = "px", dpi = 150)
 
-table(pn2021$VrstaUdelezenca, pn2021$Povzrocitelj)
+ggsave(file.path("figures", "figure-1-cyclists-age-structure-by-severity.png"),
+  width = 1000, height = 1000, units = "px", dpi = 150)
 
-table(pn2021$VrstaUdelezenca, pn2021$PoskodbaUdelezenca)
+table(rad$VrstaUdelezenca, rad$Povzrocitelj)
 
-table(pn2021$PoskodbaUdelezenca, pn2021$UporabaVarnostnegaPasu, pn2021$VrstaUdelezenca)
+table(rad$VrstaUdelezenca, rad$PoskodbaUdelezenca)
 
-pn2021.alko <- pn2021[pn2021$VrednostAlkotesta > 0, ]
+table(rad$PoskodbaUdelezenca, rad$UporabaVarnostnegaPasu, rad$VrstaUdelezenca)
+
+alko <- rad[rad$VrednostAlkotesta > 0, ]
 ggplot(
-  data = pn2021.alko,
-  mapping = aes(x = VrednostAlkotesta, y = VozniskiStalez)
+  data = alko,
+  mapping = aes(x = VozniskiStalez, y = VrednostAlkotesta)
 ) +
   theme_bw() +
-  geom_point(alpha = 0.25)
+  geom_point(alpha = 0.05) +
+  facet_wrap(~ Spol)
+
+ggsave(file.path("figures", "figure-2-experience-alcohol-gender.png"),
+width = 1000, height = 600, units = "px", dpi = 150)
+
+smrt <- rad[rad$KlasifikacijaNesrece %in% "SMRT", ]
+
+ggplot(
+  data = smrt,
+  mapping = aes(x = Spol, y = Starost)
+) +
+theme_bw() +
+geom_violin() +
+geom_jitter(alpha = 0.25, width = 0.1)
+
+ggplot(
+  data = smrt,
+  mapping = aes(x = Starost, fill = Spol)
+) +
+theme_bw() +
+theme(legend.position = "top") +
+ylab("Število") +
+xlab("Starost") +
+scale_fill_manual(values = c("darkgrey", "lightgrey")) +
+geom_histogram()
+
+ggsave(file.path("figures", "figure-3-death-by-gender-and-age.png"),
+width = 600, height = 600, units = "px", dpi = 150)
